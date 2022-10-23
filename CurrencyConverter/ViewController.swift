5dc76a2dc5ac273913ba48c7a4e1c383b3fab9e4
Sezer Turkdal <sezer.turkdal@hotmail.com>
2022-10-23 15:00:13 +0300

    User interface edited

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblCAD: UILabel!
+    @IBOutlet weak var lblCHF: UILabel!
+    @IBOutlet weak var lblGBP: UILabel!
+    @IBOutlet weak var lblJPY: UILabel!
+    @IBOutlet weak var lblNOK: UILabel!
+    @IBOutlet weak var lblUSD: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
