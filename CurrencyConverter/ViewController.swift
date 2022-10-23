1e83211be229f5c4a20dbadf02bb5bffe3627db3
Sezer Turkdal <sezer.turkdal@hotmail.com>
2022-10-23 16:06:02 +0300

    Fetching data from API

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -15,9 +15,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblJPY: UILabel!
     @IBOutlet weak var lblNOK: UILabel!
     @IBOutlet weak var lblUSD: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let accessKey = "test"
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=" + accessKey)
+        let session = URLSession.shared
+        
+        //Closure
+        let task = session.dataTask(with: url!) { data, response, error in
+            if error != nil{
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+            }else{
+                if data != nil{
+                    do{
+                    let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)
+                        
+                        DispatchQueue.main.async {
+                            print(jsonResponse )
+                        }
+                        
+                    }catch{
+                        print("Error!")
+                    }
+                }
+            }
+        }
+        task.resume()
+        
     }
 
 
